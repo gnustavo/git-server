af2fc0de5d41569186d1826660f3f277c2849e31
Gustavo L. de M. Chaves <gustavo@gnustavo.com>
2012-06-07 20:29:13 -0300

    Adjustments after first try

--- gitorious-install.sh
+++ gitorious-install.sh
@@ -19,27 +19,28 @@
 set -eu
 IFS=`printf '\n\t'`
 
-while getopts :nv OPT; do
-    case $OPT in
-	n|+n)
-	
-	    ;;
-	v|+v)
-	    set -x
-	    ;;
-	*)
-	    echo "usage: `basename $0` [+-nv} [--] ARGS..."
-	    exit 2
-    esac
-done
-shift `expr $OPTIND - 1`
-
 TMPDIR=`mktemp -d /tmp/tmp.XXXXXXXXXX` || exit 1
 trap "rm -rf $TMPDIR" EXIT
 
+# Check environment
+if [ `id -run` != git ]; then
+    echo "ERROR: You should run me as user 'git', not '`id -run`'." >&2
+    exit 1
+fi
+
+DISTRIB_OK='Ubuntu 12.04 LTS'
+source /etc/lsb-release
+if [ "$DISTRIB_DESCRIPTION" != "$DISTRIB_OK" ]; then
+    echo "ERROR: I'm prepared to run on '$DISTRIB_OK', not on '$DISTRIB_DESCRIPTION'." >&2
+    exit 1
+fi
+
+set -x
+
+# Grok configuration
 source gitorious-install.config
 
-# Pre-configure some packages
+# Pre-configure some packages to avoid manual intervention
 sudo apt-get install debconf-utils
 sudo debconf-set-selections <<EOF
 mysql-server-5.5 mysql-server/root_password_again password ${MYSQL_PWD}
@@ -64,26 +65,148 @@ cd ~/gitorious
 git submodule init
 git submodule update
 
-# Put gitorious on the user's PATH
+# Put gitorious in the user's PATH
 mkdir -p ~/bin
 ln -s ~/gitorious/script/gitorious ~/bin
 PATH=~/bin:$PATH
 
+# Disable default stompserver boot script
+sudo service stompserver stop
+sudo update-rc.d -f stompserver remove
+sudo update-rc.d stompserver stop 20 0 1 2 3 4 5 6 .
+
 # Install boot scripts
 cd ~/gitorious/doc/templates/ubuntu
-BOOT_SCRIPTS='git-daemon git-poller git-ultrasphinx stomp'
-sudo cp $BOOT_SCRIPTS /etc/init.d/
-cd /etc/init.d
-sudo chmod 755 $BOOT_SCRIPTS
 
-# Create links needed by the boot scripts
-sudo ln -s /usr /opt/ruby-enterprise
-sudo ln -s ~/gitorious /var/www/gitorious
+sudo patch -o /etc/init.d/git-daemon <<EOF
+--- git-daemon  2012-06-05 23:07:48.484013139 -0300
++++ /etc/init.d/git-daemon      2012-06-06 10:00:17.105643453 -0300
+@@ -11,11 +11,11 @@
+ 
+ # Author: Fabio Akita <fabioakita@gmail.com>
+ 
+-RUBY_HOME="/opt/ruby-enterprise"
+-GITORIOUS_HOME="/var/www/gitorious"
++RUBY_HOME="/usr"
++GITORIOUS_HOME="$HOME/gitorious"
+ RETVAL=0
+ PROG="git-daemon"
+-GIT_DAEMON="\$RUBY_HOME/bin/ruby \$GITORIOUS_HOME/script/git-daemon -d"
++GIT_DAEMON="bundle exec \$RUBY_HOME/bin/ruby \$GITORIOUS_HOME/script/git-daemon -d"
+ LOCK_FILE=/var/lock/git-daemon
+ PID_FILE=\$GITORIOUS_HOME/log/git-daemon.pid
+  
+@@ -36,7 +36,7 @@
+   do_check_pid
+   if [ \$RUNNING != 2 ] ; then
+     echo -n "Starting \$PROG: "
+-    /bin/su - git -c "\$GIT_DAEMON"
++    /bin/su - git -c "cd \$GITORIOUS_HOME && \$GIT_DAEMON"
+     sleep 5
+     if [ -f \$PID_FILE ] ; then
+       echo "success"
+EOF
+
+sudo patch -o /etc/init.d/git-poller <<EOF
+--- git-poller  2012-06-05 23:07:48.484013139 -0300
++++ /etc/init.d/git-poller      2012-06-06 10:09:43.385624623 -0300
+@@ -11,11 +11,11 @@
+ 
+ # Author: Antonio Marques <acmarques@gmail.com>
+ 
+-RUBY_HOME="/opt/ruby-enterprise"
+-GITORIOUS_HOME="/var/www/gitorious"
++RUBY_HOME="/usr"
++GITORIOUS_HOME="$HOME/gitorious"
+ RETVAL=0
+ PROG="poller"
+-GIT_POLLER="\$RUBY_HOME/bin/ruby \$GITORIOUS_HOME/script/poller"
++GIT_POLLER="bundle exec \$RUBY_HOME/bin/ruby \$GITORIOUS_HOME/script/poller"
+ LOCK_FILE=/var/lock/git-poller
+ PID_FILE=\$GITORIOUS_HOME/tmp/pids/poller0.pid
+ export RAILS_ENV=production
+@@ -40,7 +40,7 @@
+   do_check_pid
+   if [ \$RUNNING != 2 ] ; then
+     echo -n "Starting \$PROG: "
+-    /bin/su - git -c "RAILS_ENV=\$RAILS_ENV \$GIT_POLLER start"
++    /bin/su - git -c "cd \$GITORIOUS_HOME && RAILS_ENV=\$RAILS_ENV \$GIT_POLLER start"
+     sleep 4
+     if [ -f \$PID_FILE ] ; then
+       echo "Success"
+@@ -66,7 +66,7 @@
+     RETVAL=1
+   else
+     #killproc -p \$PID_FILE
+-    /bin/su - git -c "RAILS_ENV=\$RAILS_ENV \$GIT_POLLER stop"
++    /bin/su - git -c "cd \$GITORIOUS_HOME && RAILS_ENV=\$RAILS_ENV \$GIT_POLLER stop"
+     RETVAL=\$?
+     sleep 4
+   fi
+EOF
+
+sudo patch -o /etc/init.d/git-ultrasphinx <<EOF
+--- git-ultrasphinx     2012-06-05 23:07:48.484013139 -0300
++++ /etc/init.d/git-ultrasphinx 2012-06-06 10:17:54.969608274 -0300
+@@ -11,12 +11,12 @@
+ 
+ # Author: Fabio Akita <fabioakita@gmail.com>
+ 
+-GITORIOUS_HOME="/var/www/gitorious"
++GITORIOUS_HOME="$HOME/gitorious"
+ RETVAL=0
+-START_CMD="cd \$GITORIOUS_HOME && rake ultrasphinx:daemon:start RAILS_ENV=production"
+-STOP_CMD="cd \$GITORIOUS_HOME && rake ultrasphinx:daemon:stop RAILS_ENV=production"
+-RESTART_CMD="cd \$GITORIOUS_HOME && rake ultrasphinx:daemon:restart RAILS_ENV=production"
+-STATUS_CMD="cd \$GITORIOUS_HOME && rake ultrasphinx:daemon:status RAILS_ENV=production"
++START_CMD="cd \$GITORIOUS_HOME && bundle exec rake ultrasphinx:daemon:start RAILS_ENV=production"
++STOP_CMD="cd \$GITORIOUS_HOME && bundle exec rake ultrasphinx:daemon:stop RAILS_ENV=production"
++RESTART_CMD="cd \$GITORIOUS_HOME && bundle exec rake ultrasphinx:daemon:restart RAILS_ENV=production"
++STATUS_CMD="cd \$GITORIOUS_HOME && bundle exec rake ultrasphinx:daemon:status RAILS_ENV=production"
+ LOCK_FILE=/var/lock/git-ultrasphinx
+ PID_FILE=\$GITORIOUS_HOME/db/sphinx/log/searchd.pid
+  
+EOF
+
+sudo patch -o /etc/init.d/stomp <<EOF
+--- stomp       2012-06-05 23:07:48.484013139 -0300
++++ /etc/init.d/stomp   2012-06-06 10:26:05.685591956 -0300
+@@ -10,12 +10,12 @@
+ ### END INIT INFO
+ #
+ 
+-RUBY_HOME="/opt/ruby-enterprise"
++RUBY_HOME="/usr"
+ GEMS_HOME="/usr"
+-GITORIOUS_HOME="/var/www/gitorious"
++GITORIOUS_HOME="$HOME/gitorious"
+ RETVAL=0
+ PROG="stompserver"
+-STOMP="\$RUBY_HOME/bin/ruby \$GEMS_HOME/bin/stompserver -w \$GITORIOUS_HOME/tmp/stomp -q file -s queue &> /dev/null &"
++STOMP="bundle exec \$RUBY_HOME/bin/ruby \$GEMS_HOME/bin/stompserver -w \$GITORIOUS_HOME/tmp/stomp -q file -s queue &> /dev/null &"
+ LOCK_FILE=/var/lock/stomp
+ PID_FILE=\$GITORIOUS_HOME/tmp/stomp/log/stompserver.pid
+ 
+@@ -39,7 +39,7 @@
+   do_check_pid
+   if [ \$RUNNING != 2 ] ; then
+     echo -n "Starting \$PROG: "
+-    /bin/su - git -c "\$STOMP"
++    /bin/su - git -c "cd \$GITORIOUS_HOME && \$STOMP"
+     sleep 4
+     if [ -f \$PID_FILE ] ; then
+       echo "Success"
+EOF
+
+for i in git-daemon git-poller git-ultrasphinx stomp; do
+    sudo chmod 755 /etc/init.d/$i
+    sudo update-rc.d $i defaults
+done
 
 # Configure Apache2
 cat >$TMPDIR/gitorious <<EOF
 <VirtualHost *:80>
-    ServerName ${HOSTNAME}
+    ServerName ${SERVERNAME}
     DocumentRoot $HOME/gitorious/public
 </VirtualHost>
 EOF
@@ -104,6 +227,7 @@ sudo cp $TMPDIR/gitorious-ssl /etc/apache2/sites-available/
 
 sudo a2dissite 000-default
 sudo a2ensite gitorious gitorious-ssl
+sudo a2enmod ssl
 sudo service apache2 restart
 
 # Create user gitorious in MySQL
@@ -113,14 +237,14 @@ FLUSH PRIVILEGES;
 QUIT
 EOF
 
-# Install all required gems
+# Install all required Ruby gems
 cd ~/gitorious
 bundle install --path vendor/cache
 bundle pack
 
 # Create a few needed directories
 cd ~
-mkdir -p tmp/pids repositories tarballs tarballs-work
+mkdir -p gitorious/tmp/pids repositories tarballs tarballs-work
 
 # Configure Gitorious
 cd ~/gitorious/config
@@ -147,7 +271,7 @@ patch -o gitorious.yml gitorious.sample.yml <<EOF
  
    # Host name users use to reach Gitorious, e.g. "gitorious.org".
 -  gitorious_host: gitorious.test
-+  gitorious_host: ${HOSTNAME}
++  gitorious_host: ${SERVERNAME}
  
    # The path where git repositories are stored. The actual (bare) repositories
    # reside in #{repository_base_path}/#{project.slug}/#{repository.name}.git/:
@@ -169,7 +293,7 @@ patch -o gitorious.yml gitorious.sample.yml <<EOF
 +  archive_work_dir: "$HOME/tarballs-work"
  
    # Session key. It is recommended to use a long random string for this. A
-   # suitable key can be generated with `apg -m 64`. Make sure you paste the
+   # suitable key can be generated with \`apg -m 64\`. Make sure you paste the
    # key as one long string, no newlines or escaped characters.
 -  cookie_secret: ssssht
 +  cookie_secret: ${SECRET}
@@ -194,7 +318,7 @@ patch -o gitorious.yml gitorious.sample.yml <<EOF
  
    # Enabling private repositories allows users to control read-access to their
    # repositories. Repositories are public by default, but individual users
-@@ -140,7 +140,7 @@
+@@ -140,10 +140,10 @@
    # from individual repositories and/or projects.
    # More information is available in the Gitorious Wiki:
    # https://gitorious.org/gitorious/pages/PrivateRepositories
@@ -202,21 +326,55 @@ patch -o gitorious.yml gitorious.sample.yml <<EOF
 +  enable_private_repositories: true
  
    # Only site admins can create projects.
-   #only_site_admins_can_create_projects: false
+-  #only_site_admins_can_create_projects: false
++  only_site_admins_can_create_projects: true
+ 
+   # System message that will appear on all pages if present
+   #system_message:
+@@ -167,10 +167,10 @@
+   #always_display_ssh_url: false
+ 
+   # Is this gitorious.org? Read: should we have a very flashy homepage?
+-  #is_gitorious_dot_org: true
++  is_gitorious_dot_org: false
+ 
+   # Configure which address to use as From when sending email
+-  #sender_email_address: "Gitorious <no-reply@yourdomain.example>"
++  sender_email_address: "Gitorious <gustavo+gitoriousadmin@cpqd.com.br>"
+ 
+   # Mangle visible e-mail addresses (spam protection)
+   #mangle_email_addresses: true
+@@ -231,7 +231,7 @@
+   #
+ 
+   # Pick a default license
+-  #default_license: GNU Affero General Public License (AGPLv3)
++  default_license: None
+ 
+   # ----------------
+   # THEMING SETTINGS
 @@ -241,7 +241,7 @@
    #extra_html_head_data:
  
    # Email address to the support for the Gitorious server
 -  #gitorious_support_email: support@gitorious.local
-+  #gitorious_support_email: ${ADMIN_EMAIL}
++  gitorious_support_email: ${ADMIN_EMAIL}
  
    # The SSH fingerprint of your server
    #ssh_fingerprint: "7e:af:8d:ec:f0:39:5e:ba:52:16:ce:19:fa:d4:b8:7d"
+@@ -251,6 +251,7 @@
+   #additional_footer_links:
+   #  - - Professional Gitorious Services
+   #    - http://gitorious.com/
++  additional_footer_links:
+ 
+   # Set to true if you want to render terms of service and privacy policy links
+   # in the footer.
 EOF
 
 cp broker.yml.example broker.yml
 
-# Fix a bug
+# Fix a bug in Gitorious
 cd ~/gitorious/config
 patch boot.rb <<'EOF'
 --- boot.rb.orig        2012-06-03 20:11:23.570703794 -0300
@@ -232,19 +390,19 @@ patch boot.rb <<'EOF'
  module Rails
 EOF
 
-# Substitute a deprecated configuration
-patch ~/gitorious/config/ultrasphinx/production.conf <<'EOF'
---- production.conf.orig	2012-06-06 00:09:37.553350918 -0300
-+++ production.conf	2012-06-06 00:09:47.849351147 -0300
-@@ -10,7 +10,7 @@
-   pid_file = /home/git/gitorious/db/sphinx/log/searchd.pid
-   max_children = 300
-   seamless_rotate = 1
--  address = 0.0.0.0
-+  listen = 0.0.0.0
-   read_timeout = 5
-   max_matches = 100000
-   query_log = /home/git/gitorious/db/sphinx/log/query.log
+# Fix an annoying use of a deprecated method in the system Ruby
+sudo patch /usr/lib/ruby/vendor_ruby/1.8/rubygems/source_index.rb <<'EOF'
+--- source_index.rb.orig 2012-06-06 09:41:59.709679944 -0300
++++ source_index.rb      2012-06-06 09:40:39.801682601 -0300
+@@ -124,7 +124,7 @@
+         gemspec = Gem::Deprecate.skip_during do
+           Gem::Specification.load spec_file
+         end
+-        add_spec gemspec if gemspec
++        Gem::Specification.add_spec gemspec if gemspec
+       end
+     end
+ 
 EOF
 
 # Apply all configuration
@@ -254,8 +412,19 @@ bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake ultrasphinx:bootstrap
 
+# Substitute a deprecated configuration in Gitorious
+ed ~/gitorious/config/ultrasphinx/production.conf <<EOF
+/address =/
+s/address/listen/
+i
+  compat_sphinxql_magics = 0
+.
+w
+q
+EOF
+
 # Install crontab for the sphinx indexer
-crontab -l >$TMPDIR/cron
+crontab -l >$TMPDIR/cron || true
 echo '@hourly cd gitorious && RAILS_ENV=production bundle exec rake ultrasphinx:index' >>$TMPDIR/cron
 crontab $TMPDIR/cron
 
